1c568e74a853b415eda423fd4c4013849aaf6bfe
engr-OsamaAliKhan <aliosamaengr@gmail.com>
2022-12-06 15:11:26 +0500

    main

---
+++ Sumilatron/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Sumilatron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.t24c1 = new System.Windows.Forms.CheckBox();
+            this.heading = new System.Windows.Forms.Label();
+            this.host = new System.Windows.Forms.Label();
+            this.ubps = new System.Windows.Forms.Label();
+            this.ubpssim = new System.Windows.Forms.CheckBox();
+            this.bbh = new System.Windows.Forms.Label();
+            this.startbtn = new System.Windows.Forms.Button();
+            this.t24c2 = new System.Windows.Forms.CheckBox();
+            this.t24c5 = new System.Windows.Forms.CheckBox();
+            this.t24c4 = new System.Windows.Forms.CheckBox();
+            this.t24c3 = new System.Windows.Forms.CheckBox();
+            this.log = new System.Windows.Forms.Label();
+            this.ipadd = new System.Windows.Forms.Label();
+            this.selectall = new System.Windows.Forms.Button();
+            this.deselectall = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // t24c1
+            // 
+            this.t24c1.AutoSize = true;
+            this.t24c1.Location = new System.Drawing.Point(40, 91);
+            this.t24c1.Name = "t24c1";
+            this.t24c1.Size = new System.Drawing.Size(83, 17);
+            this.t24c1.TabIndex = 0;
+            this.t24c1.Text = "T24C1 Host";
+            this.t24c1.UseVisualStyleBackColor = true;
+            this.t24c1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // heading
+            // 
+            this.heading.Font = new System.Drawing.Font("Algerian", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.heading.Location = new System.Drawing.Point(92, 9);
+            this.heading.Name = "heading";
+            this.heading.Size = new System.Drawing.Size(177, 35);
+            this.heading.TabIndex = 4;
+            this.heading.Text = "Simulatron";
+            this.heading.Click += new System.EventHandler(this.heading_Click);
+            // 
+            // host
+            // 
+            this.host.AutoSize = true;
+            this.host.Location = new System.Drawing.Point(37, 60);
+            this.host.Name = "host";
+            this.host.Size = new System.Drawing.Size(103, 13);
+            this.host.TabIndex = 5;
+            this.host.Text = "Bank Host Simulator";
+            this.host.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ubps
+            // 
+            this.ubps.AutoSize = true;
+            this.ubps.Location = new System.Drawing.Point(218, 60);
+            this.ubps.Name = "ubps";
+            this.ubps.Size = new System.Drawing.Size(82, 13);
+            this.ubps.TabIndex = 6;
+            this.ubps.Text = "UBPS Simulator";
+            // 
+            // ubpssim
+            // 
+            this.ubpssim.AutoSize = true;
+            this.ubpssim.Location = new System.Drawing.Point(221, 91);
+            this.ubpssim.Name = "ubpssim";
+            this.ubpssim.Size = new System.Drawing.Size(82, 17);
+            this.ubpssim.TabIndex = 7;
+            this.ubpssim.Text = "UBPS-7777";
+            this.ubpssim.UseVisualStyleBackColor = true;
+            this.ubpssim.CheckedChanged += new System.EventHandler(this.ubpssim_CheckedChanged);
+            // 
+            // bbh
+            // 
+            this.bbh.AutoSize = true;
+            this.bbh.Location = new System.Drawing.Point(218, 120);
+            this.bbh.Name = "bbh";
+            this.bbh.Size = new System.Drawing.Size(92, 13);
+            this.bbh.TabIndex = 8;
+            this.bbh.Text = "BB Host Simulator";
+            this.bbh.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // startbtn
+            // 
+            this.startbtn.Location = new System.Drawing.Point(221, 251);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(98, 23);
+            this.startbtn.TabIndex = 10;
+            this.startbtn.Text = "Start";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
+            // t24c2
+            // 
+            this.t24c2.AutoSize = true;
+            this.t24c2.Location = new System.Drawing.Point(40, 119);
+            this.t24c2.Name = "t24c2";
+            this.t24c2.Size = new System.Drawing.Size(83, 17);
+            this.t24c2.TabIndex = 11;
+            this.t24c2.Text = "T24C2 Host";
+            this.t24c2.UseVisualStyleBackColor = true;
+            this.t24c2.CheckedChanged += new System.EventHandler(this.t24c2_CheckedChanged);
+            // 
+            // t24c5
+            // 
+            this.t24c5.AutoSize = true;
+            this.t24c5.Location = new System.Drawing.Point(40, 152);
+            this.t24c5.Name = "t24c5";
+            this.t24c5.Size = new System.Drawing.Size(83, 17);
+            this.t24c5.TabIndex = 12;
+            this.t24c5.Text = "T24C5 Host";
+            this.t24c5.UseVisualStyleBackColor = true;
+            this.t24c5.CheckedChanged += new System.EventHandler(this.t24c3_CheckedChanged);
+            // 
+            // t24c4
+            // 
+            this.t24c4.AutoSize = true;
+            this.t24c4.Location = new System.Drawing.Point(40, 187);
+            this.t24c4.Name = "t24c4";
+            this.t24c4.Size = new System.Drawing.Size(83, 17);
+            this.t24c4.TabIndex = 13;
+            this.t24c4.Text = "T24C4 Host";
+            this.t24c4.UseVisualStyleBackColor = true;
+            this.t24c4.CheckedChanged += new System.EventHandler(this.t24c4_CheckedChanged);
+            // 
+            // t24c3
+            // 
+            this.t24c3.AutoSize = true;
+            this.t24c3.Location = new System.Drawing.Point(221, 152);
+            this.t24c3.Name = "t24c3";
+            this.t24c3.Size = new System.Drawing.Size(65, 17);
+            this.t24c3.TabIndex = 14;
+            this.t24c3.Text = "BB Host";
+            this.t24c3.UseVisualStyleBackColor = true;
+            this.t24c3.CheckedChanged += new System.EventHandler(this.bbhost_CheckedChanged);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(333, 152);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(177, 73);
+            this.log.TabIndex = 15;
+            this.log.Click += new System.EventHandler(this.log_Click);
+            // 
+            // ipadd
+            // 
+            this.ipadd.Location = new System.Drawing.Point(359, 251);
+            this.ipadd.Name = "ipadd";
+            this.ipadd.Size = new System.Drawing.Size(141, 23);
+            this.ipadd.TabIndex = 16;
+            this.ipadd.Click += new System.EventHandler(this.ipadd_Click);
+            // 
+            // selectall
+            // 
+            this.selectall.Location = new System.Drawing.Point(221, 222);
+            this.selectall.Name = "selectall";
+            this.selectall.Size = new System.Drawing.Size(97, 23);
+            this.selectall.TabIndex = 17;
+            this.selectall.Text = "Select All";
+            this.selectall.UseVisualStyleBackColor = true;
+            this.selectall.Click += new System.EventHandler(this.selectall_Click);
+            // 
+            // deselectall
+            // 
+            this.deselectall.Location = new System.Drawing.Point(40, 222);
+            this.deselectall.Name = "deselectall";
+            this.deselectall.Size = new System.Drawing.Size(81, 23);
+            this.deselectall.TabIndex = 18;
+            this.deselectall.Text = "Deselect All";
+            this.deselectall.UseVisualStyleBackColor = true;
+            this.deselectall.Click += new System.EventHandler(this.deselectall_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(40, 250);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(81, 23);
+            this.close.TabIndex = 19;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 309);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.deselectall);
+            this.Controls.Add(this.selectall);
+            this.Controls.Add(this.ipadd);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.t24c3);
+            this.Controls.Add(this.t24c4);
+            this.Controls.Add(this.t24c5);
+            this.Controls.Add(this.t24c2);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.bbh);
+            this.Controls.Add(this.ubpssim);
+            this.Controls.Add(this.ubps);
+            this.Controls.Add(this.host);
+            this.Controls.Add(this.heading);
+            this.Controls.Add(this.t24c1);
+            this.Name = "Form1";
+            this.Text = "Simulatron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox t24c1;
+        private System.Windows.Forms.Label heading;
+        private System.Windows.Forms.Label host;
+        private System.Windows.Forms.Label ubps;
+        private System.Windows.Forms.CheckBox ubpssim;
+        private System.Windows.Forms.Label bbh;
+        private System.Windows.Forms.Button startbtn;
+        private System.Windows.Forms.CheckBox t24c2;
+        private System.Windows.Forms.CheckBox t24c5;
+        private System.Windows.Forms.CheckBox t24c4;
+        private System.Windows.Forms.CheckBox t24c3;
+        private System.Windows.Forms.Label log;
+        private System.Windows.Forms.Label ipadd;
+        private System.Windows.Forms.Button selectall;
+        private System.Windows.Forms.Button deselectall;
+        private System.Windows.Forms.Button close;
+    }
+}
+
---
+++ Sumilatron/Form1.cs
@@ -0,0 +1,438 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Winium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using System.IO;
+using AutoIt;
+using OpenQA.Selenium.Interactions;
+using WindowsInput.Native;
+using WindowsInput;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Sumilatron
+{
+    
+    public partial class Form1 : Form
+    {
+        
+
+        OracleConnection conn = new OracleConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        private static void ClearTextByElement(IWebElement element)
+        {
+            int existingCount = element.Text.Length;
+            for (int i = 0; i < existingCount; i++)
+            { element.Clear(); }
+        }
+        public static void VerifyTextonTextBox(IWebElement element, string Text)
+        {
+
+
+            if (element.Text != Text)
+            {
+                ClearTextByElement(element);
+                element.SendKeys(Text);
+
+            }
+        }
+        public static void Close_all(IWebDriver driver) {
+
+            driver.Dispose();
+        }
+        public static void Click(IWebDriver driver, IWebElement element)
+        {
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(40.00));
+
+            //  IWait<IWebDriver> wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40.00));
+            // wait.IgnoreExceptionTypes(new[] { typeof(StaleElementReferenceException), typeof(NoSuchElementException) });
+
+            try
+            {
+                //   wait.Until(driver2 =>
+
+                // element.Displayed == true);
+            }
+            catch { }
+            Actions Ac = new Actions(driver);
+            Ac.Click(element).Build().Perform();
+            Thread.Sleep(1000);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10.00));
+
+        }
+
+        public static void SelectFromDropDown(string ApplicationName, string ComboBox, string Value)
+        {
+            AutoItX.WinActivate(ApplicationName, "");
+
+            AutoItX.ControlCommand(ApplicationName, "", ComboBox, "SelectString", Value);
+
+        }
+        String sim1 = "";
+        String sim2 = "";
+        String sim3 = "";
+        String sim4 = "";
+        String sim5 = "";
+        String sim6 = "";
+        String exePath = "D:/Simulatron/ABLHOST/Simulatrix.exe";
+        String ubp = "D:/Simulatron/UBPS SIMULATOR/UBPSSIM.exe";
+
+  
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void heading_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ubpssim_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            String ipaddr = GetLocalIPAddress();
+            InputSimulator sim = new InputSimulator();
+            ipadd.Text = ipaddr;
+            
+            DesktopOptions options = new DesktopOptions();
+
+            if (t24c1.Checked == true)
+            {
+                sim1 = "T24C1 Checked ";
+                log.Text = sim1;
+
+
+               
+                String updtip = $"update TBLNETWORK set COMMSADDRESS='{ipaddr}' where NETNAME like 'T24C1' or NETNAME like 'T24C2'or NETNAME like 'T24C3'or NETNAME like 'T24C4'or NETNAME like 'T24C5'or NETNAME like 'UBPS-7777' ";
+                OracleCommand cmd2 = new OracleCommand(updtip, conn);
+                OracleDataReader rd2 = cmd2.ExecuteReader();
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'T24C1'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn); 
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = exePath;
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(2000);
+          
+
+
+
+                AutoItX.WinActivate("Log-In");
+                driver.FindElement(By.Name("Admin 1 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn1").Click();
+                driver.FindElement(By.Name("Admin 2 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn2").Click();
+                driver.FindElementById("Close").Click();
+
+                AutoItX.WinActivate("Simulatrix -- HOST PHX8583 [HBL]");
+                Thread.Sleep(1000);
+                IWebElement wind=driver.FindElementByName("Simulatrix -- HOST PHX8583 [HBL]");
+                IWebElement toolbr = wind.FindElement(By.Id("MainToolStrip"));
+               // toolbr.Click();
+                IWebElement box = toolbr.FindElement(By.ClassName("WindowsForms10.EDIT.app.0.378734a"));
+                box.Click();
+                Thread.Sleep(500);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "{TAB}");
+                Thread.Sleep(500);
+                AutoItX.Send(port);
+              
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "!{c}");
+
+
+
+
+
+
+
+            }
+            if (t24c2.Checked == true)
+            {
+
+                sim2 = "T24C2 Checked ";
+                log.Text = sim1+sim2;
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'T24C2'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn);
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = exePath;
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(3000);
+
+                AutoItX.WinActivate("Log-In");
+                driver.FindElement(By.Name("Admin 1 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn1").Click();
+                driver.FindElement(By.Name("Admin 2 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn2").Click();
+                driver.FindElementById("Close").Click();
+                AutoItX.WinActivate("Simulatrix -- HOST PHX8583 [HBL]");
+                Thread.Sleep(2000);
+                IWebElement wind = driver.FindElementByName("Simulatrix -- HOST PHX8583 [HBL]");
+                IWebElement toolbr = wind.FindElement(By.Id("MainToolStrip"));
+                // toolbr.Click();
+                IWebElement box = toolbr.FindElement(By.ClassName("WindowsForms10.EDIT.app.0.378734a"));
+                box.Click();
+                Thread.Sleep(1000);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "{TAB}");
+                Thread.Sleep(1000);
+                AutoItX.Send(port);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "!{c}");
+
+              
+
+
+
+
+            }
+            if(t24c5.Checked == true)
+            {
+                sim3 = "T24C5 Checked ";
+                log.Text = sim1+ sim2+sim3;
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'T24C5'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn);
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = exePath;
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(3000);
+
+                AutoItX.WinActivate("Log-In");
+                driver.FindElement(By.Name("Admin 1 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn1").Click();
+                driver.FindElement(By.Name("Admin 2 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn2").Click();
+                driver.FindElementById("Close").Click();
+                AutoItX.WinActivate("Simulatrix -- HOST PHX8583 [HBL]");
+                Thread.Sleep(2000);
+                IWebElement wind = driver.FindElementByName("Simulatrix -- HOST PHX8583 [HBL]");
+                IWebElement toolbr = wind.FindElement(By.Id("MainToolStrip"));
+                // toolbr.Click();
+                IWebElement box = toolbr.FindElement(By.ClassName("WindowsForms10.EDIT.app.0.378734a"));
+                box.Click();
+                Thread.Sleep(1000);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "{TAB}");
+                Thread.Sleep(1000);
+                AutoItX.Send(port);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "!{c}");
+
+
+
+            }
+            if (t24c4.Checked == true)
+            {
+                sim4 = "T24C4 Checked ";
+                log.Text = sim1+ sim2 + sim3+sim4;
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'T24C4'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn);
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = exePath;
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(3000);
+
+                AutoItX.WinActivate("Log-In");
+                driver.FindElement(By.Name("Admin 1 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn1").Click();
+                driver.FindElement(By.Name("Admin 2 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn2").Click();
+                driver.FindElementById("Close").Click();
+                AutoItX.WinActivate("Simulatrix -- HOST PHX8583 [HBL]");
+                Thread.Sleep(2000);
+                IWebElement wind = driver.FindElementByName("Simulatrix -- HOST PHX8583 [HBL]");
+                IWebElement toolbr = wind.FindElement(By.Id("MainToolStrip"));
+                // toolbr.Click();
+                IWebElement box = toolbr.FindElement(By.ClassName("WindowsForms10.EDIT.app.0.378734a"));
+                box.Click();
+                Thread.Sleep(1000);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "{TAB}");
+                Thread.Sleep(1000);
+                AutoItX.Send(port);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "!{c}");
+
+
+            }
+            if (t24c3.Checked == true)
+            {
+                sim5 = "BB Host Checked ";
+                log.Text = sim1+ sim2 + sim3 + sim4+sim5;
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'T24C3'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn);
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = exePath;
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(3000);
+
+                AutoItX.WinActivate("Log-In");
+                driver.FindElement(By.Name("Admin 1 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn1").Click();
+                driver.FindElement(By.Name("Admin 2 Password")).SendKeys("tpstps123123");
+                driver.FindElementById("LogIn2").Click();
+                driver.FindElementById("Close").Click();
+                AutoItX.WinActivate("Simulatrix -- HOST PHX8583 [HBL]");
+                Thread.Sleep(2000);
+                IWebElement wind = driver.FindElementByName("Simulatrix -- HOST PHX8583 [HBL]");
+                IWebElement toolbr = wind.FindElement(By.Id("MainToolStrip"));
+                // toolbr.Click();
+                IWebElement box = toolbr.FindElement(By.ClassName("WindowsForms10.EDIT.app.0.378734a"));
+                box.Click();
+                Thread.Sleep(1000);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "{TAB}");
+                Thread.Sleep(1000);
+                AutoItX.Send(port);
+                AutoItX.ControlSend("Simulatrix -- HOST PHX8583 [HBL]", "", "", "!{c}");
+
+
+            }
+            if (ubpssim.Checked == true)
+            {
+                sim6 = "UBPS Checked ";
+                log.Text = sim1+sim2+sim3+sim4+sim5+sim6;
+                String hp = "select COMMS_PORT from TBLNETWORK where NETNAME like 'UBPS-7777'";
+                OracleCommand cmd1 = new OracleCommand(hp, conn);
+                OracleDataReader rd1 = cmd1.ExecuteReader();
+                rd1.Read();
+                string port = rd1.GetString(0);
+
+                options.ApplicationPath = ubp;
+
+                WiniumDriver driver = new WiniumDriver(@"D:\Simulatron\Driver\Winium.Desktop.Driver", options);
+
+                Thread.Sleep(1000);
+                IWebElement sts = driver.FindElementByName("Bill Status");
+                sts.FindElement(By.Name("UnPaid Bill")).Click();
+
+                var grpCs=driver.FindElementByName("Connection Settings");
+                grpCs.FindElement(By.Id("textBoxLP")).SendKeys(port);
+
+                grpCs.FindElement(By.Name("Start")).Click();
+
+
+            }
+            conn.Close();
+            
+        }
+
+        private void t24c2_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void t24c3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void t24c4_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void bbhost_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void log_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ipadd_Click(object sender, EventArgs e)
+        {
+            String ipaddr = GetLocalIPAddress();
+            
+        }
+
+        private void selectall_Click(object sender, EventArgs e)
+        {
+            t24c1.Checked = true;
+            t24c2.Checked = true;
+            t24c5.Checked = true;
+            t24c4.Checked = true;
+            t24c3.Checked = true;
+            ubpssim.Checked = true;
+        }
+
+        private void deselectall_Click(object sender, EventArgs e)
+        {
+            t24c1.Checked = false;
+            t24c2.Checked = false;
+            t24c5.Checked = false;
+            t24c4.Checked = false;
+            t24c3.Checked = false;
+            ubpssim.Checked = false;
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
